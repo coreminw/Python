4655b0857e181a8702b7fa76bb2c24ed3a12f759
coreminw <coreminw@naver.com>
2022-06-08 20:34:22 +0900

    재귀함수

---
+++ 0608_9week.py
@@ -0,0 +1,19 @@
+# 9주차
+# 재귀호출
+
+
+# n! = n x (n-1) x (n-2) x ... x 1
+#    = n x (n-1)!
+def factorial(n):
+    ft = 1
+    for i in range(n, 0, -1):
+        ft *= i
+    return ft
+
+def factorial2(n):
+    if n < 1 and n >= 0:
+        return 1 #재귀호출 탈출 조건
+    else:
+        return n * factorial(n - 1)
+print(factorial(10))
+print(factorial2(10))
