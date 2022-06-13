1cb33eb77bba6dce8021c19a4a2ae4976ba764a3
coreminw <coreminw@naver.com>
2022-06-13 21:13:00 +0900

    인코딩 예외처리 추가

--- 0613_13weeks.py
+++ 0613_13weeks.py
@@ -4,6 +4,8 @@ try:
     fname = "/Users/kimminwook/PycharmProjects/pythonProgramming/venv/0609_9weeks.py"
     f = open(fname)
 except FileNotFoundError:
-    print(fname + "파일이 없습니다. 파일 이름을 다시 확인하세요")
+    print(fname + " 파일이 없습니다. 파일 이름을 다시 확인하세요.")
+except UnicodeDecodeError:
+    print(fname + " 파일 저장 인코딩 방법을 확인해주세요.")
 except:
-    print("Could not open "+ fname)
+    print("다른 종류의 오류 발생 "+ fname)
