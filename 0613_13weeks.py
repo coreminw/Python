6069ff145794ddbdfb7d72ed47457e8bd26d66c0
coreminw <coreminw@naver.com>
2022-06-13 21:09:40 +0900

    FileNotFoundError 예외처리 추가

--- 0613_13weeks.py
+++ 0613_13weeks.py
@@ -3,5 +3,7 @@
 try:
     fname = "/Users/kimminwook/PycharmProjects/pythonProgramming/venv/0609_9weeks.py"
     f = open(fname)
+except FileNotFoundError:
+    print(fname + "파일이 없습니다. 파일 이름을 다시 확인하세요")
 except:
-    print("Could not open ", fname)
+    print("Could not open "+ fname)
